fe9c593c88ea0b8eda3e6e719ce3270886ff8f6a
Shu-OMEN <shuboeiw@gmail.com>
2020-10-27 15:10:51 +0800

    instance variable

---
+++ .history/puzzle4/puzzle4_20201027145352.java
@@ -0,0 +1,9 @@
+public class puzzle4 {
+    public static void main(String[] args){
+
+    }
+}
+
+public static void main(String[] args){
+    ''
+}
---
+++ .history/puzzle4/puzzle4b_20201027150931.java
@@ -0,0 +1,10 @@
+class puzzle4b{
+    int ivar;
+    public int doStuff(int factor){
+        if(ivar>100){
+            return ivar * factor;
+        }else{
+            return ivar * (5 - factor);
+        }
+    }
+}
