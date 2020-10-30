d0ac819774162840b9f996407a1982566a2ea2f7
Shu-OMEN <shuboeiw@gmail.com>
2020-10-30 20:43:59 +0800

    demo2

---
+++ simpleDotComTestDrive/DotCom.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> loc){
+        locationCells = loc;
+    }
+
+
+    public String checkYourself(String userInput){
+        String result = "miss";
+        
+        int index = locationCells.indexOf(userInput);
+        if(index >= 0){
+            locationCells.remove(index);
+
+            if(locationCells.isEmpty()){
+                result = "kill";
+            }else{
+                result = "hit";
+            }
+        }
+		return result;
+    }
+}
