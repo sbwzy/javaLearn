3eb722ab0f69d2b7a4f0457bd93f3f6d35de80ab
Shu-OMEN <shuboeiw@gmail.com>
2020-11-17 15:10:11 +0800

    Mixed2

---
+++ extend/.history/Mixed2_20201117143844.java
@@ -0,0 +1,10 @@
+class A {
+    int ivar = 7;
+    void m1(){
+        System.out.println("A's m1, ");
+    }
+}
+
+public class Mixed2 {
+    
+}
---
+++ extend/.history/Mixed2_20201117143942.java
@@ -0,0 +1,13 @@
+class A {
+    int ivar = 7;
+    void m1(){
+        System.out.println("A's m1, ");
+    }
+    void m2(){
+        System.out.println("A's m2, ");
+    }
+}
+
+public class Mixed2 {
+    
+}
