394e898a231c3e4b4492b6536b62fb0ebfa12ed9
Shu-OMEN <shuboeiw@gmail.com>
2020-11-18 15:46:43 +0800

    构造函数

---
+++ UseADuck/.history/UseADuck_20201118154531.java
@@ -0,0 +1,11 @@
+public class Duck {
+    public Duck(){
+        System.out.println("Quark!");
+    }
+}
+
+public class UseADuck{
+    public static void main(String[] args){
+        Duck d = new Duck();
+    }
+}
