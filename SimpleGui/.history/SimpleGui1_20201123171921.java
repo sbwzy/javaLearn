801c78184b6bf9b250fcf7490a8035da3e2b506c
Shu-OMEN <shuboeiw@gmail.com>
2020-11-23 19:06:24 +0800

    clicked

---
+++ SimpleGui/.history/SimpleGui1_20201123171921.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SimpleGui1{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 在windows关闭时结束掉程序
+
+        frame.getContentPane().add(button);
+
+        frame.setSize(300,300);
+
+        frame.setVisible(true); // 显示frame
+    }
+}
