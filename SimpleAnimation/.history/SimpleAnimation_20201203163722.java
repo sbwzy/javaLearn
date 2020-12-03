6c4bd1a180875e3762400a204298fd6c54fb9116
Shu-OMEN <shuboeiw@gmail.com>
2020-12-03 16:38:31 +0800

    SimpleAnimation

--- SimpleAnimation/.history/SimpleAnimation_20201203163722.java
+++ SimpleAnimation/.history/SimpleAnimation_20201203163722.java
@@ -37,7 +37,7 @@ public class SimpleAnimation {
 
         public void paintComponent(Graphics g) {
             g.setColor(Color.white);
-            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+            g.fillRect(90, 90, this.getWidth(), this.getHeight());
 
             g.setColor(Color.green);
             g.fillOval(x, y, 40, 40);
