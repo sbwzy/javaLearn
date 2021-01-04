9d667ba321a048098ce286a37e0d912bab6e0b19
Shu-OMEN <shuboeiw@gmail.com>
2021-01-04 22:09:15 +0800

    DailyAdviceServer

---
+++ DailyAdviceClient/.history/DailyAdviceServer_20210104215614.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+public class DailyAdviceServer {
+    String[] adviceList = {"Take smaller bites", "Go for the tight jeans. No they do Not make you look fat.",
+"One word: inappropriate", "Just for today, be honest. Tell your boss what you *really* think",
+"You might want to rethink that haircut"};
+    public void go() {
+        try {
+            ServerSocket serverSock = new ServerSocket(4242);
+            
+        }
+    }
+}
