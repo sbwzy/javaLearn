13bbb4dd074bada7d7991c3aad408a7d1ef08dc4
Shu-OMEN <shuboeiw@gmail.com>
2021-01-10 22:01:48 +0800

    Thread

---
+++ RyanAndMonicaJob/.history/RyanAndMonicaJob_20210110214857.java
@@ -0,0 +1,41 @@
+class BankAccount {
+    private int balance = 100;
+    public int getBalance() {
+        return balance;
+    }
+    public void withdraw(int amount) {
+        balance = balance-amount;
+    }
+}
+
+public class RyanAndMonicaJob implements Runnable {
+    private BankAccount account = new BankAccount();
+
+    public static void main (String[] args) {
+        RyanAndMonicaJob theJob = new RyanAndMonicaJob();
+        Thread one = new Thread(theJob);
+        Thread two = new Thread(theJob);
+        one.setName("Ryan");
+        two.setName("Monica");
+        one.start();
+        two.start();
+    }
+
+    public void run() {
+        for(int x=0; x<10; x++) {
+            makeWithdrawl(10);
+            if(account.getBalance() < 0) {
+                System.out.println("Overdraw");
+            }
+        }
+    }
+
+    private void makeWithdrawl(int amount) {
+        if(account.getBalance() >= amount) {
+            System.out.println(Thread.currentThread().getName() + " is about to withdraw");
+            try {
+                System.out.println(Thread.currentThread().getName() + " is about to sleep");
+            }
+        }
+    }
+}
