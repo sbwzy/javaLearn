b732d385d33552fb9541725402df9e4be5044943
Shu-OMEN <shuboeiw@gmail.com>
2021-03-15 15:48:35 +0800

    SimpleChatClient

---
+++ SimpleChatClient/.history/SimpleChatClient_20210315153611.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SimpleChatClient {
+    JTextField outgoing;
+    JTextArea incoming;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+
+    public static void main (String[] args) {
+        SimpleChatClient client = new SimpleChatClient();
+        client.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        incoming = new JTextArea(15, 50);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetworking();
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+    }
+
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 5000);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                writer.print(outgoing.getText());
+                writer.flush();
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+}
